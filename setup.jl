2099dfd5fc64e127bdaaa37abf88ae7f419ee334
Yiannis Simillides <yiannis.simillides.14@ucl.ac.uk>
2019-03-11 14:49:28 +0000

    Update setup.jl

--- setup.jl
+++ setup.jl
@@ -1,6 +1,6 @@
 using Pkg
+ENV["PYTHON"] = ""
 Pkg.clone("https://github.com/JuliaPy/PyCall.jl")
-ENV["PYTHON"] = "/usr/bin/python3"
 Pkg.build("PyCall")
 Pkg.clone("https://github.com/wildart/TOML.jl")
 Pkg.clone("https://github.com/alan-turing-institute/MLJBase.jl")
